451e72d1ac99df3fcbfc79bf284f3821b649f863
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-04 15:34:42 -0500

    redid format as per array in directions

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -4,6 +4,67 @@ count = 0;
 score.textContent = count;
 $("#question").html("");
 
+var questions = [
+    {
+      title: "What goes on the end of every statement in JavaScript?",
+      choices: [";", ".", ":", "="],
+      answer: ";"
+    },
+    {
+      title: "What does '==' mean in JavaScript?",
+      choices: ["sets a variable", "strictly equal (value and type the same)", "equal in value but not necessarily in type", "comment"],
+      answer: "strictly equal (value and type the same)"
+    },
+    {
+        title: "What does console.log() do in JavaScript?",
+        choices: ["calls a function", "displays a value in the console", "provides the logarithm of a numeric value", "converts a statement to a string"],
+        answer: "displays a value in the console"
+
+    },
+    {
+        title: "Which is not a statement name?",
+        choices: ["if", "else if", "else", "ifthen"],
+        answer: "ifthen"
+
+    },
+    {
+        title: "What defines a variable?",
+        choices: ["v", "variable", "var", "function"],
+        answer: "var"
+
+    },
+    {
+        title: "Which of these means 'and'?",
+        choices: ["||", "&&", "::", "//"],
+        answer: "&&"
+
+    },
+    {
+        title: "Which of these means 'or'?",
+        choices: ["||", "&&", "::", "//"],
+        answer: "||"
+
+    },
+    {
+        title: "What do JavaScript files end with?",
+        choices: [".java", ".ja", ".jv", ".js"],
+        answer: ".js"
+
+    },
+    {
+        title: "What does parseInt() do?",
+        choices: ["turns value into an integer", "integrates a function", "turns a value into a string", "nothing"],
+        answer: "turns value into an integer"
+
+    },
+    {
+        title: "Which of the following starts a jQuery statement?",
+        choices: ["$", "@", "#", "/"],
+        answer: "$"
+
+    },
+  ];
+
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
@@ -26,7 +87,16 @@ $(".start-button").click(function() {
         display = document.querySelector('#time');
     startTimer(fiveMinutes, display);
     document.getElementById("start-button").style.visibility = "hidden";
-    $("#question").html("1. What goes on the end of every statement in JavaScript?");
+    $("#question").text(questions[0].title);
+    $("#letter_a").text(questions[0].choices[0]);
+    $("#letter_b").text(questions[0].choices[1]);
+    $("#letter_c").text(questions[0].choices[2]);
+    $("#letter_d").text(questions[0].choices[3]);
+    if
+
+    })
+});
+  /*  $("#question").html("1. What goes on the end of every statement in JavaScript?");
     $("#letter_a").html("/");
     $("#letter_b").html(".");
     $("#letter_c").html(";");
@@ -48,16 +118,8 @@ $(".start-button").click(function() {
         count += 4;
         localStorage.setItem("count", count);
         score.textContent = count;
-      });
-      document.getElementById("ornament_d").addEventListener("click", function(){
-        alert("That's not correct. Try again.");
-        count -= 1;
-        localStorage.setItem("count", count);
-        score.textContent = count;
-      });
-    });
-/*
-function question2(){
+
+        
     $("#question").html("2. What does '==' mean in JavaScript?");
     $("#letter_a").html("sets a variable");
     $("#letter_b").html("strictly equal (value and type the same)");
@@ -94,7 +156,16 @@ function question2(){
           localStorage.setItem("count", count);
           score.textContent = count;
       });
-    }
+
+      });
+      document.getElementById("ornament_d").addEventListener("click", function(){
+        alert("That's not correct. Try again.");
+        count -= 1;
+        localStorage.setItem("count", count);
+        score.textContent = count;
+      });
+    });
+
 
 function question3(){
  $("#question").html("3. What does 'console.log()' do in JavaScript?");
