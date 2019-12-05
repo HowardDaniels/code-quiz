0ab8c1ff3c4283c5729fc25ae1c3bc7042cfcdf7
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-05 00:09:03 -0500

    unsuccessfully attempted to stop the clock, will need to work more on this tomorrow

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -19,7 +19,28 @@ function nextQuestion(){
     alert("Game over!");
     function stopTimer(){
       if ( seconds !== 0 || minutes !== 0 || hours !== 0 ) { /* display the full time before reseting the stop watch */ 
-        var fulltime = document .getElementById( "fulltime" ); //display the full time fulltime.style.display = "block"; var time = gethours + mins + secs; fulltime.innerHTML = 'Fulltime: ' + time; // reset the stop watch seconds = 0; minutes = 0; hours = 0; secs = '0' + seconds; mins = '0' + minutes + ': '; gethours = '0' + hours + ': '; /* display the stopwatch after it's been stopped */ var x = document.getElementById ("timer"); var stopTime = gethours + mins + secs; x.innerHTML = stopTime; /* display all stop watch control buttons */ var showStart = document.getElementById ('start'); showStart.style.display = "inline-block"; var showStop = document.getElementById ("stop"); showStop.style.display = "inline-block"; /* clear the stop watch using the setTimeout( ) return value 'clearTime' as ID */ clearTimeout( clearTime ); } // if () } // stopTime() /* you need to call the stopTime( ) function to terminate the stop watch */ window.addEventListener( 'load', function ( ) { var stop = document.getElementById ("stop"); stop.addEventListener( 'click', stopTime ); }); // stopwatch.js end 
+        var fulltime = document .getElementById("time"); 
+        fulltime.style.display = "block"; 
+        var time = gethours + mins + secs; 
+        fulltime.innerHTML = 'Fulltime: ' + time; 
+        seconds = 0; 
+        minutes = 0; 
+        hours = 0;
+        secs = '0' + seconds; 
+        mins = '0' + minutes + ': '; 
+        gethours = '0' + hours + ': '; 
+        var x = document.getElementById ("time"); 
+        var stopTime = gethours + mins + secs; 
+        x.innerHTML = stopTime; 
+        var showStart = document.getElementById ('start'); 
+        showStart.style.display = "inline-block"; 
+        var showStop = document.getElementById ("stop"); 
+        showStop.style.display = "inline-block"; 
+        clearTimeout( clearTime ); } 
+        window.addEventListener( 'load', function ( ) { 
+          var stop = document.getElementById ("stop"); 
+          stop.addEventListener( 'click', stopTime ); 
+        }); 
     }
   }
 }
